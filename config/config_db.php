e80ae8b08dc570654f21581d842debbf51eec57b
PaoCar <pao_cardona97@hotmail.com>
2018-05-10 16:56:09 -0500

    save

--- config/config_db.php
+++ config/config_db.php
@@ -2,6 +2,6 @@
 class DB extends DBmysql {
    public $dbhost     = 'localhost';
    public $dbuser     = 'root';
-   public $dbpassword = '';
+   public $dbpassword = 'gwcpd';
    public $dbdefault  = 'bd_soluciones';
 }
